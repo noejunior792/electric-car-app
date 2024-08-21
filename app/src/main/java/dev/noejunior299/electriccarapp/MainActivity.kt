674017a32ba9ebd0152227f017f8be9037761f05
Noé Júnior <noedombaxe@gmail.com>
2024-08-21 18:41:34 +0100

    🔥feat: calculate the vehicle autonomy

--- app/src/main/java/dev/noejunior299/electriccarapp/MainActivity.kt
+++ app/src/main/java/dev/noejunior299/electriccarapp/MainActivity.kt
@@ -3,26 +3,44 @@ package dev.noejunior299.electriccarapp
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
 
 class MainActivity : ComponentActivity() {
 
+    lateinit var preco: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var btnCalcular: Button
+    lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.layout)
+        setupView()
         setupListeners()
     }
 
-    fun setupListeners() {
-        val preco = findViewById<EditText>(R.id.et_precoCalculo)
-        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+    fun setupView() {
+        btnCalcular = findViewById(R.id.btn_calcular)
+        preco = findViewById(R.id.et_precoCalculo)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        resultado = findViewById(R.id.tv_resultado)
+    }
 
+    fun setupListeners() {
         btnCalcular.setOnClickListener {
-            val textoDigitado = preco.text.toString()
-            println(textoDigitado)
+            calcular()
         }
+    }
+
+    fun calcular() {
+        val preco = preco.text.toString().toFloat()
+        val kmPercorrido = kmPercorrido.text.toString().toFloat()
+
+        val result = preco / kmPercorrido
+        resultado.text = result.toString()
     }
 }
