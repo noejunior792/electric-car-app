edfd0dbb791f6af6a1e722ee3377f76015a12ddb
Noé Júnior <noedombaxe@gmail.com>
2024-08-21 18:00:41 +0100

    🔥feat: add function for receive value variable of screen

--- app/src/main/java/dev/noejunior299/electriccarapp/MainActivity.kt
+++ app/src/main/java/dev/noejunior299/electriccarapp/MainActivity.kt
@@ -1,15 +1,28 @@
 package dev.noejunior299.electriccarapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContentView(R.layout.layout)
+        setupListeners()
+    }
+
+    fun setupListeners() {
+        val preco = findViewById<EditText>(R.id.et_precoCalculo)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val textoDigitado = preco.text.toString()
+            println(textoDigitado)
+        }
     }
 }
