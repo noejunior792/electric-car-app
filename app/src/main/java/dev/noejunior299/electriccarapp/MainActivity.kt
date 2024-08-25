c82514106c3bcb5cd4bd80d6331024a46ee86829
Noé Júnior <noedombaxe@gmail.com>
2024-08-25 18:40:29 +0100

    ⚙️refactor: separate calculation responsibilities from autonomy

--- app/src/main/java/dev/noejunior299/electriccarapp/MainActivity.kt
+++ app/src/main/java/dev/noejunior299/electriccarapp/MainActivity.kt
@@ -12,10 +12,7 @@ import dev.noejunior299.electriccarapp.presentation.CalcularAutonomiaActivity
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var preco: EditText
-    lateinit var kmPercorrido: EditText
     lateinit var btnCalcular: Button
-    lateinit var resultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,23 +24,11 @@ class MainActivity : ComponentActivity() {
 
     fun setupView() {
         btnCalcular = findViewById(R.id.btn_calcular)
-        preco = findViewById(R.id.et_precoCalculo)
-        kmPercorrido = findViewById(R.id.et_km_percorrido)
-        resultado = findViewById(R.id.tv_resultado)
     }
 
     fun setupListeners() {
         btnCalcular.setOnClickListener {
-            // calcular()
             startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
         }
-    }
-
-    fun calcular() {
-        val preco = preco.text.toString().toFloat()
-        val kmPercorrido = kmPercorrido.text.toString().toFloat()
-
-        val result = preco / kmPercorrido
-        resultado.text = result.toString()
     }
 }
--- app/src/main/java/dev/noejunior299/electriccarapp/presentation/CalcularAutonomiaActivity.kt
+++ app/src/main/java/dev/noejunior299/electriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -2,13 +2,51 @@ package dev.noejunior299.electriccarapp.presentation
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import dev.noejunior299.electriccarapp.R
 
 class CalcularAutonomiaActivity:  ComponentActivity() {
 
+    lateinit var preco: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var btnCalcular: Button
+    lateinit var resultado: TextView
+    lateinit var btnClose: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_calcular_autonomia)
+        setupView()
+        setupListeners()
     }
+
+    fun setupView() {
+        btnCalcular = findViewById(R.id.btn_calcular)
+        preco = findViewById(R.id.et_precoCalculo)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        resultado = findViewById(R.id.tv_resultado)
+        btnClose = findViewById(R.id.imageView)
+    }
+
+    fun setupListeners() {
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+        btnClose.setOnClickListener {
+            finish()
+        }
+    }
+
+    fun calcular() {
+        val preco = preco.text.toString().toFloat()
+        val kmPercorrido = kmPercorrido.text.toString().toFloat()
+
+        val result = preco / kmPercorrido
+        resultado.text = result.toString()
+    }
+
 }
